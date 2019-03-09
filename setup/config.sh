b7991219a641c356268ff1ca067db4a00a9c61b0
Morteza Nourelahi Alamdari <me@mortezana.com>
2019-03-10 00:59:35 +0100

    Update setup files and emacs config

--- setup/config.sh
+++ setup/config.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+exit  # leave root user
+
+# emacs
+cp ../emacs/emacs ~/.emacs
+emacs -nw --eval="(package-install-selected-packages)"
+
+# tmux
+cp ../tmux/tmux.conf ~/.tmux.conf
+
 # set terminal theme (dconf)
 
 # update dock settings (bottom, max size)
--- setup/fonts.sh
+++ setup/fonts.sh
@@ -2,3 +2,5 @@
 
 wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/DejaVuSansMono.zip
 unzip DejaVuSansMono.zip
+mv *.ttf ~/.local/share/fonts
+fc-cache -f -r -v
