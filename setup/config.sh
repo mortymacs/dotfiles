5f74c5861fac98bf0ba6aa7f9c8ede4c917e930d
Morteza Nourelahi Alamdari <me@mortezana.com>
2019-03-10 00:39:45 +0100

    Add config and font bash files.

---
+++ setup/config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# set terminal theme (dconf)
+
+# update dock settings (bottom, max size)
+
+# update dock to default icons and orders
+
+# install gnome extensions
+
+# set timezone and language config
+
+# install gnome theme and set as default
+
+# set default hot keys (super + enter -> terminal , ...)
+
+# set default fonts (mono, ...)
---
+++ setup/fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/DejaVuSansMono.zip
+unzip DejaVuSansMono.zip
