3e2f677b3a507a912ea5a8a666a5d3d309e5d78d
Morteza NourelahiAlamdari <me@mortezana.com>
2019-11-28 10:21:10 +0100

    Update emacs theme config

--- emacs/.emacs.d/theme.el
+++ emacs/.emacs.d/theme.el
@@ -39,6 +39,8 @@
 ;; ---------------------------------------------- line
 (set-face-attribute 'hl-line nil :background "#262626")
 (set-face-foreground 'highlight nil)
+;; https://stackoverflow.com/a/18685171/2338672
+(set-face-attribute 'region nil :background "#333")
 
 ;; ---------------------------------------------- mode-line
 ;; https://emacs.stackexchange.com/a/33091/19615
