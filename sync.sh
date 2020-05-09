81652157f9d37403a132c6bd1f2c1190cce1a3d6
Morteza NourelahiAlamdari <m@0t1.me>
2020-05-10 01:37:27 +0200

    Update packages and add sync script

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# zsh
+cp ~/{.zshrc,.zsh_aliases,.p10k.zsh} zsh/
+
+# tmux
+cp ~/.tmux.conf tmux/
+
+# emacs
+cp ~/.emacs ./emacs/
+cp /home/morteza/.emacs.d/{functions.el,keybindings.el,theme.el} emacs/.emacs.d/
+
+# gdb
+cp ~/.gdbinit gdb/
+
+# git
+cp ~/{.gitconfig,.gitignore} git/
+
+# tig
+cp ~/.tigrc tig/
+
+# packages
+pacman -Qqen > pacman/packages.txt
