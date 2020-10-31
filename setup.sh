2ed897db97ad1fe3c7baa6a588602f79b6c140c5
Morteza NourelahiAlamdari <m@0t1.me>
2020-10-31 12:05:08 +0100

    update dotfiles

---
+++ setup.sh
@@ -0,0 +1,4 @@
+> xdg-user-dirs-update --set WORKSPACE ~/Workspace
+> xdg-user-dirs-update --set APPLICATIONS ~/Applications
+
+cat ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com/metadata.json | jq .name,.url
