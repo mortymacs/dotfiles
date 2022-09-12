95cf8a19b9af4f48b5a4c2cd8f638ea2ef3284f2
Morteza NourelahiAlamdari <m@0t1.me>
2022-09-12 12:06:40 +0200

    Update dotfiles

--- sync.sh
+++ sync.sh
@@ -91,7 +91,7 @@ cp "$HOME/.config/Zeal/Zeal.conf" zeal/
 
 # fonts
 rm -rf fonts/*
-cp -R "$HOME"/.local/share/fonts/{Code\ New\ Roman\ Nerd\ Font\ Complete\ Mono.otf,Code\ New\ Roman\ Bold\ Nerd\ Font\ Complete\ Mono.otf,Code\ New\ Roman\ Italic\ Nerd\ Font\ Complete\ Mono.otf,'AnekLatin-VariableFont_wdth,wght.ttf'} fonts/
+cp -R "$HOME"/.local/share/fonts/{Code\ New\ Roman\ Nerd\ Font\ Complete\ Mono.otf,Code\ New\ Roman\ Bold\ Nerd\ Font\ Complete\ Mono.otf,Code\ New\ Roman\ Italic\ Nerd\ Font\ Complete\ Mono.otf} fonts/
 
 # push changes.
 git status -s
