bfff09b9e3a7b11e5c0b278ef47fbe7e90b4efd5
Morteza NourelahiAlamdari <m@0t1.me>
2022-12-28 13:48:38 +0100

    Update dotfiles

--- nvim/lua/vim.lua
+++ nvim/lua/vim.lua
@@ -1,24 +1,43 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.syntax = "on"
-vim.opt.mouse = ""
-vim.opt.backspace = "2"
+-- Body
 vim.opt.nu = true
 vim.opt.rnu = true
-vim.opt.cursorline = true
+
+-- File, syntax and theme
+vim.opt.filetype = "on"
 vim.opt.compatible = false
+vim.opt.syntax = "on"
+vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.hidden = true
+vim.opt.background = "dark"
+
+-- Backup
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.updatetime = 300
-vim.opt.signcolumn = "yes"
+
+-- Indentation
 vim.opt.cindent = true
-vim.opt.smartindent = true
 vim.opt.autoindent = true
-vim.opt.filetype = "on"
-vim.opt.wrap = false
-vim.opt.background = "dark"
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.backspace = "2"
+
+-- Menu
 vim.opt.completeopt = "menu,menuone,noselect"
+
+-- Wrap
+vim.opt.wrap = false
+-- https://superuser.com/a/559436
+vim.opt.whichwrap:append("<,>,h,l,[,]")
+
+-- Mouse
+vim.opt.mouse = ""
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Misc
+vim.opt.hidden = true
+vim.opt.signcolumn = "yes"
