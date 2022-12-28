cf5b99f62b67aee9bb17e967d00ba8d68d64027f
Morteza NourelahiAlamdari <m@0t1.me>
2022-12-28 21:22:25 +0100

    Update dotfiles

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -2,26 +2,34 @@ vim.cmd.colorscheme("spaceduck")
 vim.g.lightline = "{'colorscheme': 'spaceduck'}"
 
 -- Body
-vim.api.nvim_set_hl(0, "Normal", {bg = ""})
+vim.api.nvim_set_hl(0, "Normal",      {bg = ""})
 vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "", fg = "#0f0f0f"})
-vim.api.nvim_set_hl(0, "LineNr", {bg = "", fg = "#686f9a"})
-vim.api.nvim_set_hl(0, "SignColumn", {bg = ""})
-vim.api.nvim_set_hl(0, "VertSplit", {bg = "", fg = "#1b1c36"})
-vim.api.nvim_set_hl(0, "NonText", {bg = "", fg = ""})
+vim.api.nvim_set_hl(0, "LineNr",      {bg = "", fg = "#686f9a"})
+vim.api.nvim_set_hl(0, "SignColumn",  {bg = ""})
+vim.api.nvim_set_hl(0, "VertSplit",   {bg = "", fg = "#1b1c36"})
+vim.api.nvim_set_hl(0, "NonText",     {bg = "", fg = ""})
 
 -- Git
-vim.api.nvim_set_hl(0, "GitSignsAdd", {bg = "", fg = "#5ccc96"})
+vim.api.nvim_set_hl(0, "GitSignsAdd",    {bg = "", fg = "#5ccc96"})
 vim.api.nvim_set_hl(0, "GitSignsChange", {bg = "", fg = "#e39400"})
 vim.api.nvim_set_hl(0, "GitSignsDelete", {bg = "", fg = "#ce6f8f"})
 
 -- Terminal
 vim.api.nvim_set_hl(0, "FloatermBorder", {fg = "#1b1c36"})
 
+-- Notification
+vim.api.nvim_set_hl(0, "NotifyDEBUGBody",  {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "NotifyINFOBody",   {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "NotifyWARNBody",   {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "NotifyERRORBody",  {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "NotifyTRACEBody",  {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "NotifyBackground", {bg = "#0f0f0f"})
+
 -- Telescope
-vim.api.nvim_set_hl(0, "TelescopeNormal", {bg = ""})
-vim.api.nvim_set_hl(0, "TelescopePreviewNormal", {bg = ""})
-vim.api.nvim_set_hl(0, "TelescopePromptNormal", {bg = ""})
-vim.api.nvim_set_hl(0, "TelescopeResultNormal", {bg = ""})
+vim.api.nvim_set_hl(0, "TelescopeNormal",        {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "TelescopePromptNormal",  {bg = "#0f0f0f"})
+vim.api.nvim_set_hl(0, "TelescopeResultNormal",  {bg = "#0f0f0f"})
 
 -- Bookmark
 vim.g.bookmark_sign = ''
