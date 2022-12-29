097492861956a2a643f9620cc4c4dc4c559e4dff
Morteza NourelahiAlamdari <m@0t1.me>
2022-12-29 21:05:30 +0100

    Update dotfiles

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -11,4 +11,4 @@ vim.g.floaterm_keymap_next   = "<c-t><Right>"
 vim.g.floaterm_keymap_kill   = "<c-t><c-k>"
 vim.g.floaterm_opener = "tabe"
 -- https://stackoverflow.com/a/63908546/2338672
-vim.api.nvim_create_autocmd("TermOpen", {command = "setlocal nonu nornu"})
+vim.api.nvim_create_autocmd("TermOpen", {command = "setlocal nonu nornu signcolumn=no"})
--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -1,7 +1,11 @@
 function SetKeyMap(shortcut, command, modes)
-	modes = modes or {"n", "i", "v"}
+	modes = modes or {"n", "i", "v", "x", "o", "c", "t", "l"}
 	for k, v in pairs(modes) do
         pcall(vim.keymap.del, v, shortcut)
-        vim.keymap.set(v, shortcut, command, {noremap = true, silent = true})
+        if v == "n" then
+            vim.keymap.set(v, shortcut, command, {silent = true, noremap = true})
+        else
+            vim.keymap.set(v, shortcut, command, {silent = true, noremap = false})
+        end
 	end
 end
