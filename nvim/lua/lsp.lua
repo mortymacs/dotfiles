0b68114d28fc694433f0225b8ff14bd515555c2a
Morteza NourelahiAlamdari <m@0t1.me>
2023-01-24 23:09:18 +0100

    Update dotfiles

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -14,6 +14,20 @@ require("fidget").setup({
 -- Lua
 require("neodev").setup()
 
+-- Diagnostic
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = false,
+  update_in_insert = true,
+  severity_sort = false,
+})
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 -- Icon
 local kind_icons = {
   Text = '',
