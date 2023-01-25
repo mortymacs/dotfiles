54b7ff633120f13a32840b0cd638c20842d2a527
Morteza NourelahiAlamdari <m@0t1.me>
2023-01-25 23:20:44 +0100

    Update dotfiles

--- nvim/lua/statusbar.lua
+++ nvim/lua/statusbar.lua
@@ -2,7 +2,7 @@
 require('lualine').setup({
   options = {
     theme = "ayu_dark",
-    section_separators = {left = "", right = ""},
+    section_separators = {left = "", right = ""},
     component_separators = "",
     disabled_filetypes = {
       statusline = {"NvimTree"},
