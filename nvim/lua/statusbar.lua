7fd6179b833fca2482376967aae9d70403d68be7
Morteza NourelahiAlamdari <m@0t1.me>
2023-01-29 23:00:14 +0100

    Update dotfiles

--- nvim/lua/statusbar.lua
+++ nvim/lua/statusbar.lua
@@ -2,7 +2,7 @@
 require('lualine').setup({
   options = {
     theme = "ayu_mirage",
-    section_separators = {left = "", right = ""},
+    section_separators = {left = "", right = ""},
     component_separators = "",
     globalstatus = true,
   },
