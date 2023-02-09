f8b5eed5f84d78183e00d499eaf41721f7887c8a
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-09 20:01:16 +0100

    Update dotfiles

--- nvim/lua/text.lua
+++ nvim/lua/text.lua
@@ -26,3 +26,4 @@ require('mini.indentscope').setup({
     },
     symbol = "│",
 })
+vim.api.nvim_create_autocmd("FileType", { pattern = "alpha", command = ":lua vim.b.miniindentscope_disable=true" })
