153cbdb075a4e28f7048291429cc3f391f4b4d6b
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-14 16:46:07 +0100

    Update dotfiles

--- nvim/lua/debug.lua
+++ nvim/lua/debug.lua
@@ -1,15 +1,15 @@
 local dap = require("dap")
 require("dapui").setup()
 
+-- Go
 dap.adapters.delve = {
-  type = 'server',
-  port = '${port}',
+  type = "server",
+  port = "${port}",
   executable = {
-    command = 'dlv',
-    args = {'dap', '-l', '127.0.0.1:${port}'},
+    command = "dlv",
+    args = { "dap", "-l", "127.0.0.1:${port}" },
   }
 }
-
 dap.configurations.go = {
   {
     type = "delve",
@@ -31,4 +31,55 @@ dap.configurations.go = {
     mode = "test",
     program = "./${relativeFileDirname}"
   }
+}
+
+-- CPP
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/bin/lldb-vscode',
+  name = 'lldb'
+}
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+  },
+}
+
+-- C
+dap.configurations.c = dap.configurations.cpp
+
+-- Rust
+dap.configurations.rust = dap.configurations.cpp
+
+-- Python
+dap.adapters.python = {
+  type = "executable",
+  command = "python",
+  args = { "-m", "debugpy.adapter" },
+}
+dap.configurations.python = {
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    pythonPath = function()
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+        return cwd .. "/venv/bin/python"
+      elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+        return cwd .. "/.venv/bin/python"
+      else
+        return "/usr/bin/python"
+      end
+    end,
+  },
 }
