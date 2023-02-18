252533cd5558c25a8cce1eda844ab21171a6f51f
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-18 16:50:06 +0100

    Update dotfiles

--- nvim/lua/statusbar.lua
+++ nvim/lua/statusbar.lua
@@ -1,21 +1,33 @@
 -- Lualine
-require('lualine').setup({
+require("lualine").setup({
   options = {
     theme = "ayu_mirage",
-    section_separators = {left = "", right = ""},
+    section_separators = { left = "", right = "" },
     component_separators = "",
     globalstatus = true,
   },
   sections = {
-    lualine_a = {},
-    lualine_b = {{"filename", newfile_status = true, path = 1}, "branch", "diff" },
+    lualine_a = {
+      {
+        "filename",
+        newfile_status = true,
+        path = 1,
+        symbols = {
+          modified = "[+]",
+          readonly = "[-]",
+          unnamed = "[No Name]",
+          newfile = "[New]",
+        },
+      }
+    },
+    lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {"filetype", "filesize"},
-    lualine_z = {"progress"},
+    lualine_y = { "filetype", "filesize" },
+    lualine_z = { "progress" },
   },
 })
-vim.cmd("set cmdheight=1")
+vim.cmd("set cmdheight=0")
 vim.cmd("set laststatus=3")
 
 -- Trouble
