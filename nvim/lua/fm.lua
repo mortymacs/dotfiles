645852e1399a0f45914a7192c5167bc875988ec1
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-23 18:46:48 +0100

    Update dotfiles

--- nvim/lua/fm.lua
+++ nvim/lua/fm.lua
@@ -5,109 +5,118 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require('nvim-tree').setup({
-  sync_root_with_cwd = true,
-  respect_buf_cwd = true,
-  disable_netrw = true,
-  hijack_cursor = true,
-  renderer = {
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-    },
-    special_files = {"Makefile", "CMakeLists.txt", "Cargo.toml", "Magefile", "go.mod", "go.sum", "pyproject.toml"},
-  },
-  filters = {
-    custom = {"^\\.git", "^\\.venv"},
-  },
-  live_filter = {
-    prefix = "[FILTER]: ",
-    always_show_folders = false,
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    show_on_dirs = false,
-    timeout = 400,
-  },
-  view = {
-    hide_root_folder = true,
-    signcolumn = "no",
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = "<cr>",       action = "edit" },
-        { key = "v",          action = "vsplit" },
-        { key = "h",          action = "split" },
-        { key = "<tab>",      action = "preview" },
-        { key = "g",          action = "refresh" },
-        { key = "n",          action = "create" },
-        { key = "d",          action = "remove" },
-        { key = "r",          action = "rename" },
-        { key = "m",          action = "cut" },
-        { key = "c",          action = "copy" },
-        { key = "p",          action = "paste" },
-        { key = "bp",         action = "bulk_move" },
-        { key = "y",          action = "copy_name" },
-        { key = "Y",          action = "copy_absolute_path" },
-        { key = "/",          action = "live_filter" },
-        { key = "<c-f>",      action = "search_node" },
-        { key = "<c-]>",      action = "close" },
-      },
-    },
-  },
-  renderer = {
-    icons = {
-      padding = " ",
-      show = {
-        folder_arrow = true,
-      },
-      glyphs = {
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
+    sync_root_with_cwd = true,
+    respect_buf_cwd = true,
+    disable_netrw = true,
+    hijack_cursor = true,
+    renderer = {
+        icons = {
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
         },
-      },
+        special_files = { "Makefile", "CMakeLists.txt", "Cargo.toml", "Magefile", "go.mod", "go.sum", "pyproject.toml" },
     },
-    indent_markers = {
-      enable = true,
-      inline_arrows = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        bottom = "─",
-        none = " ",
-      },
+    filters = {
+        custom = { "^\\.git", "^\\.venv" },
     },
-  },
-  actions = {
-    open_file = {
-      resize_window = false,
+    live_filter = {
+        prefix = "[FILTER]: ",
+        always_show_folders = false,
     },
-  },
+    git = {
+        enable = true,
+        ignore = false,
+        show_on_dirs = true,
+        timeout = 400,
+    },
+    view = {
+        hide_root_folder = true,
+        signcolumn = "no",
+        mappings = {
+            custom_only = true,
+            list = {
+                { key = "<cr>",  action = "edit" },
+                { key = "v",     action = "vsplit" },
+                { key = "h",     action = "split" },
+                { key = "<tab>", action = "preview" },
+                { key = "g",     action = "refresh" },
+                { key = "n",     action = "create" },
+                { key = "d",     action = "remove" },
+                { key = "r",     action = "rename" },
+                { key = "m",     action = "cut" },
+                { key = "c",     action = "copy" },
+                { key = "p",     action = "paste" },
+                { key = "bp",    action = "bulk_move" },
+                { key = "y",     action = "copy_name" },
+                { key = "Y",     action = "copy_absolute_path" },
+                { key = "/",     action = "live_filter" },
+                { key = "<c-f>", action = "search_node" },
+                { key = "<c-]>", action = "close" },
+            },
+        },
+    },
+    renderer = {
+        icons = {
+            padding = " ",
+            show = {
+                folder_arrow = true,
+            },
+            glyphs = {
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "",
+                },
+            },
+        },
+        indent_markers = {
+            enable = true,
+            inline_arrows = true,
+            icons = {
+                corner = "└",
+                edge = "│",
+                item = "│",
+                bottom = "─",
+                none = " ",
+            },
+        },
+    },
+    actions = {
+        open_file = {
+            resize_window = false,
+        },
+    },
 })
 
 -- Telescope
 require("telescope").setup({
-  defaults = {
-    layout_strategy = 'vertical',
-    borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└'},
-  },
-  pickers = {
-    find_files = {
-      borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└'},
+    defaults = {
+        layout_strategy = 'vertical',
+        borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
     },
-  },
+    pickers = {
+        find_files = {
+            borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+        },
+    },
 })
 
 require('telescope').load_extension("vim_bookmarks")
