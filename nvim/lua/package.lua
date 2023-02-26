47db19eeeaf97178f4a2caf6a6254f1894c137e9
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-26 16:07:43 +0100

    Update dotfiles

--- nvim/lua/package.lua
+++ nvim/lua/package.lua
@@ -42,13 +42,9 @@ require("lazy").setup({
     {
         "folke/noice.nvim",
         dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
-        }
+        },
     },
 
     -- Notification
