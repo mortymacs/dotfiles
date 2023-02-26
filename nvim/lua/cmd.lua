54c6d2a435af26ab5bbb9236d213693d241c4a81
Morteza NourelahiAlamdari <m@0t1.me>
2023-02-26 17:52:58 +0100

    Update dotfiles

--- nvim/lua/cmd.lua
+++ nvim/lua/cmd.lua
@@ -1,41 +1,46 @@
 require("noice").setup({
-  lsp = {
-    signature = {
-      enabled = false,
+    lsp = {
+        signature = {
+            enabled = false,
+        },
+        hover = {
+            enabled = false,
+        },
     },
-    hover = {
-      enabled = false,
+    messages = {
+        enabled = false,
     },
-  },
-  messages = {
-    enabled = false,
-  },
-  popupmenu = {
-    enabled = false,
-  },
-  cmdline = {
-    format = {
-      cmdline = { icon = ">" },
-      lua = { icon = ">" },
+    popupmenu = {
+        enabled = false,
     },
-  },
-  presets = {
-    bottom_search = true,
-    command_palette = true,
-    long_message_to_split = true,
-    inc_rename = false,
-    lsp_doc_border = false,
-  },
-  views = {
-    cmdline_popup = {
-      size = {
-        width = 100,
-        height = "auto",
-      },
-      border = {
-        style = "single",
-        padding = { 0, 0, },
-      },
+    cmdline = {
+        format = {
+            cmdline = { icon = ">" },
+            lua = { icon = ">" },
+        },
     },
-  },
+    lsp = {
+        progress = {
+            enabled = false,
+        },
+    },
+    presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+    },
+    views = {
+        cmdline_popup = {
+            size = {
+                width = 100,
+                height = "auto",
+            },
+            border = {
+                style = "single",
+                padding = { 0, 0, },
+            },
+        },
+    },
 })
