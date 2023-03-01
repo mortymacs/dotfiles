7cb3c3ccc3774fdf9a4aec5fb55512e39750c63d
Morteza NourelahiAlamdari <m@0t1.me>
2023-03-01 20:42:18 +0100

    Update dotfiles

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -19,7 +19,6 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 -- Tags
 vim.g.fzf_tags_command = "fd | ctags -R --links=no -L-"
