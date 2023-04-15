14c24bc75627fe7e29fd9144c26995d4d77016ae
Morteza NourelahiAlamdari <m@0t1.me>
2023-04-15 15:04:58 +0200

    Update alacritty config

--- home-manager/main/alacritty/default.nix
+++ home-manager/main/alacritty/default.nix
@@ -1,96 +1,96 @@
- {
-  	programs.alacritty = {
-  	enable = true;
-	settings = {
-        env = {
-            WINIT_X11_SCALE_FACTOR = "1";
-            TERM = "xterm-256color";
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env = {
+        WINIT_X11_SCALE_FACTOR = "1";
+        TERM = "xterm-256color";
+      };
+      window = {
+        opacity = 0.9;
+        decorations = "full";
+        dynamic_padding = false;
+        padding = {
+          x = 0;
+          y = 0;
         };
-		window = {
-			opacity = 0.9;
-  			decorations = "full";
-  			dynamic_padding = false;
-  			padding = {
-    				x = 0;
-    				y = 0;
-			};
-		};
+      };
+
+      cursor = {
+        style = {
+          shape = "Beam";
+          blinking = "Always";
+          blink_interval = 100;
+          vi_mode_style = "Beam";
+        };
+      };
+
+      mouse = {
+        hide_when_typing = true;
+      };
+
+      selection = {
+        save_to_clipboard = true;
+      };
+
+      font = {
+        size = 14;
+        normal = {
+          family = "Ellograph CF Neovim Test 2";
+        };
+
+        offset = {
+          x = 0;
+          y = 0;
+        };
+
+        glyph_offset = {
+          x = 0;
+          y = 0;
+        };
+      };
+
+      # https://github.com/pineapplegiant/spaceduck-terminal
+      # Space Duck
+      colors = {
+        # Default colors
+        primary = {
+          background = "#060606";
+          foreground = "#ecf0c1";
+        };
 
         cursor = {
-            style = {
-                shape = "Beam";
-                blinking = "Always";
-                blink_interval = 100;
-                vi_mode_style = "Beam";
-            };
+          cursor = "#4B3F72";
         };
 
-		mouse = {
-			hide_when_typing = true;
-		};
-
-		selection = {
-			save_to_clipboard = true;
-		};
-
-		font = {
-			size = 14;
-            normal = {
-                family = "Ellograph CF Neovim Test 2";
-            };
-
-            offset = {
-                x = 0;
-                y = 0;
-            };
-
-            glyph_offset = {
-                x = 0;
-                y = 0;
-            };
+        selection = {
+          text = "#ecf0c1";
+          background = "#1F2041";
         };
 
-		# https://github.com/pineapplegiant/spaceduck-terminal
-		# Space Duck
-		colors = {
-  			# Default colors
-  			primary = {
-    				background = "#060606";
-    				foreground = "#ecf0c1";
-			};
-
-  			cursor = {
-    				cursor =    "#4B3F72";
-			};
-
-  			selection = {
-   				text =       "#ecf0c1";
-    				background = "#1F2041";
-  			};
-  			# Normal colors
-  			normal = {
-    				black =    "#000000";
-    				red =     "#e33400";
-    				green =   "#5ccc96";
-    				yellow =  "#b3a1e6";
-    				blue =    "#00a3cc";
-    				magenta = "#f2ce00";
-    				cyan =    "#7a5ccc";
-    				white =   "#686f9a";
-			};
-  			# Bright colors
-  			bright = {
-    				black =   "#686f9a";
-    				red =     "#e33400";
-    				green =   "#5ccc96";
-    				yellow =  "#b3a1e6";
-    				blue =    "#00a3cc";
-    				magenta = "#f2ce00";
-    				cyan =    "#7a5ccc";
-    				white =   "#f0f1ce";
-
-			};
-		};
-		};
- 	};
-  }
+        # Normal colors
+        normal = {
+          black = "#000000";
+          red = "#e33400";
+          green = "#5ccc96";
+          yellow = "#b3a1e6";
+          blue = "#00a3cc";
+          magenta = "#f2ce00";
+          cyan = "#7a5ccc";
+          white = "#f0f1ce";
+        };
+        # Bright colors
+        bright = {
+          black = "#000000";
+          red = "#e33400";
+          green = "#5ccc96";
+          yellow = "#b3a1e6";
+          blue = "#00a3cc";
+          magenta = "#f2ce00";
+          cyan = "#7a5ccc";
+          white = "#f0f1ce";
+        };
+      };
+    };
+  };
+}
