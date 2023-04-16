5e3163fd6406e1a2df709588507df83b064dd1f3
Morteza NourelahiAlamdari <m@0t1.me>
2023-04-16 13:15:33 +0200

    Update picom

--- home-manager/main/picom/default.nix
+++ home-manager/main/picom/default.nix
@@ -1,8 +1,13 @@
 {
   services.picom = {
     enable = true;
+    backend = "glx";
     shadow = true;
     activeOpacity = 0.9;
     inactiveOpacity = 0.9;
+    shadowExclude = [
+      "window_type *= 'menu'"
+      "focused = 1"
+    ];
   };
 }
