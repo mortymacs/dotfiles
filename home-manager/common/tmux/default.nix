fb64b5c57239630a79ec0fab057e44908c7d4ad2
Morteza NourelahiAlamdari <m@0t1.me>
2023-05-18 13:15:15 +0200

    Update dotfiles

--- home-manager/common/tmux/default.nix
+++ home-manager/common/tmux/default.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 let
+    background = "#0d1b2a";
+    foreground = "#faf3dd";
+    left_background = "#b33f62";
+    left_foreground = "#faf3dd";
+    active_pane_background = "#f9564f";
     left_icon = "";
     right_icon = "";
 in
@@ -34,21 +39,21 @@ in
         set -g status-justify "left"
         set -g status "on"
         set -g status-left-style "none"
-        set -g message-command-style "fg=#0d1321,bg=#3e5c76"
+        set -g message-command-style "fg=${background},bg=${active_pane_background}"
         set -g status-right-style "none"
-        set -g status-style "none,bg=#0d1321"
-        set -g pane-active-border-style "fg=#5ccc96"
-        set -g message-style "fg=#0f111b,bg=#b3a1e6"
-        set -g pane-border-style "fg=#3e5c76"
+        set -g status-style "none,bg=${background}"
+        set -g pane-active-border-style "fg=${background}"
+        set -g message-style "fg=${foreground},bg=${background}"
+        set -g pane-border-style "fg=${background}"
         set -g status-right-length "100"
         set -g status-left-length "100"
         setw -g window-status-activity-style "none"
         setw -g window-status-separator ""
-        setw -g window-status-style "none,fg=#3e5c76,bg=#30365F"
-        set -g status-left "#[fg=#f0ebd8,bg=#1d2d44] #S #[fg=#1d2d44,bg=#0d1321,nobold,nounderscore,noitalics]${left_icon}"
-        set -g status-right "#[fg=#0d1321,bg=#0d1321,nobold,nounderscore,noitalics]${right_icon}#[fg=#3e5c76,bg=#0d1321] #h #{tmux_mode_indicator} "
-        setw -g window-status-format "#[fg=#3e5c76,bg=#0d1321] #I #[fg=#3e5c76,bg=#0d1321] #W "
-        setw -g window-status-current-format "#[fg=#0d1321,bg=#3e5c76,nobold,nounderscore,noitalics]${left_icon}#[fg=#0d1321,bg=#3e5c76] #I #[fg=#0d1321,bg=#3e5c76] #W #[fg=#3e5c76,bg=#0d1321,nobold,nounderscore,noitalics]${left_icon}"
+        setw -g window-status-style "none,fg=${active_pane_background},bg=${background}"
+        set -g status-left "#[fg=${left_foreground},bg=${left_background}] #S #[fg=${left_background},bg=${background},nobold,nounderscore,noitalics]${left_icon}"
+        set -g status-right "#[fg=${background},bg=${background},nobold,nounderscore,noitalics]${right_icon}#[fg=${active_pane_background},bg=${background}] #h #{tmux_mode_indicator} "
+        setw -g window-status-format "#[fg=${active_pane_background},bg=${background}] #I #[fg=${active_pane_background},bg=${background}] #W "
+        setw -g window-status-current-format "#[fg=${background},bg=${active_pane_background},nobold,nounderscore,noitalics]${left_icon}#[fg=${background},bg=${active_pane_background}] #I #[fg=${background},bg=${active_pane_background}] #W #[fg=${active_pane_background},bg=${background},nobold,nounderscore,noitalics]${left_icon}"
         '';
         plugins = with pkgs; [
             tmuxPlugins.fuzzback
