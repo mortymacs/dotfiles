937e899e556090bfe2347c9c78d4197ca0b32e4a
Morteza NourelahiAlamdari <m@0t1.me>
2023-06-13 15:22:06 +0200

    Update dotfiles

--- home-manager/common/zsh/aliases.nix
+++ home-manager/common/zsh/aliases.nix
@@ -62,6 +62,7 @@
   http-server = ''(){
             python -m http.server "$1" & firefox http://localhost:"$1"
         }'';
+  http-test-server = "docker run --rm -d -p 9090:80 kennethreitz/httpbin";
   code-capture = ''(){
             silicon "$1" -o $(basename "$\{1%%.*\}.png") --no-window-controls --font "CodeNewRoman Nerd Font Mono" --background '#fff0' --theme Coldark-Dark
         }'';
