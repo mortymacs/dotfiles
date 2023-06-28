ca8db299f1d3410e7902e2dc5d31a08a0aab2afb
Morteza NourelahiAlamdari <m@0t1.me>
2023-06-28 14:59:55 +0200

    Update dotfiles

--- home-manager/common/gtk/default.nix
+++ home-manager/common/gtk/default.nix
@@ -1,33 +1,31 @@
+let
+  extraConfig = {
+    gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+    gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+    gtk-decoration-layout = appmenu:none;
+    gtk-button-images = 1;
+    gtk-menu-images = 1;
+    gtk-enable-event-sounds = 0;
+    gtk-enable-input-feedback-sounds = 0;
+    gtk-xft-antialias = 1;
+    gtk-xft-hinting = 1;
+    gtk-xft-hintstyle = "hintfull";
+    gtk-error-bell = 0;
+  };
+in
 {
   # GTK.
   gtk = {
     enable = true;
-    iconTheme = {
-      name = "Yaru-purple-dark";
+    theme.name = "Yaru-purple-dark";
+    iconTheme.name = "Yaru-purple-dark";
+    cursorTheme.name = "Yaru";
+    font = {
+      name = "Lexend";
+      size = 10;
     };
-    theme = {
-      name = "Yaru-purple-dark";
-    };
-    gtk3 = {
-      extraConfig = {
-        gtk-theme-name = "Yaru-purple-dark";
-        gtk-icon-theme-name = "Yaru-purple-dark";
-        gtk-font-name = "Lexend 10";
-        gtk-cursor-theme-name = "Yaru";
-        gtk-cursor-theme-size = 0;
-        gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
-        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
-        gtk-decoration-layout = appmenu:none;
-        gtk-button-images = 1;
-        gtk-menu-images = 1;
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintfull";
-        gtk-error-bell = 0;
-      };
-    };
+    gtk3.extraConfig = extraConfig;
+    gtk4.extraConfig = extraConfig;
   };
 
   # Custom settings.
