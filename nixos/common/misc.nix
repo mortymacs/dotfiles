a26d681a528335875dd2a803f23e2d2105200a41
Morteza NourelahiAlamdari <m@0t1.me>
2023-07-23 11:02:03 +0200

    Update dotfiles

--- nixos/common/misc.nix
+++ nixos/common/misc.nix
@@ -1,7 +1,10 @@
-{
-  services = {
-    udisks2 = {
-      enable = true;
-    };
-  };
+{ pkgs, ... }: {
+  # Mount storages.
+  services = { udisks2 = { enable = true; }; };
+
+  # Backlight.
+  programs = { light = { enable = true; }; };
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
+  '';
 }
