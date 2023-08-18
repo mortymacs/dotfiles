2bcd6f913ce6b84e4610c7d61eceb25176eb3e3b
Morteza NourelahiAlamdari <m@0t1.me>
2023-08-18 13:24:29 +0200

    Update dotfiles

--- home-manager/common/picom/default.nix
+++ home-manager/common/picom/default.nix
@@ -6,13 +6,14 @@
     fade = false;
     activeOpacity = 0.9;
     inactiveOpacity = 0.9;
-    shadowExclude = [ "window_type *= 'menu'" ];
+    shadowExclude = [ "window_type *= 'menu'" "window_type *= 'utility'" ];
     settings = {
       corner-radius = 0;
       rounded-corners-exclude = [
-        "window_type = 'menu'"
-        "window_type = 'dropdown_menu'"
-        "window_type = 'popup_menu'"
+        "window_type *= 'menu'"
+        "window_type *= 'utility'"
+        "window_type *= 'dropdown_menu'"
+        "window_type *= 'popup_menu'"
         "class_g = 'Polybar'"
         "class_g = 'Rofi'"
         "class_g = 'Dunst'"
@@ -22,6 +23,8 @@
         size = 10;
         deviation = 5.0;
       };
+      blur-background-exclude =
+        [ "window_type *= 'menu'" "window_type *= 'utility'" ];
     };
   };
 }
