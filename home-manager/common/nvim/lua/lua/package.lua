8bb74e53a26e206f3c9875355cf77ac931994f2b
Morteza NourelahiAlamdari <m@0t1.me>
2023-08-30 17:41:47 +0200

    Update dotfiles

--- home-manager/common/nvim/lua/lua/package.lua
+++ home-manager/common/nvim/lua/lua/package.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     -- Theme.
     "ayu-theme/ayu-vim",
+    "pineapplegiant/spaceduck",
     "mvllow/modes.nvim",
 
     -- Tabbar.
--- home-manager/common/nvim/lua/lua/statusline.lua
+++ home-manager/common/nvim/lua/lua/statusline.lua
@@ -1,40 +1,38 @@
 -- Dependencies.
-local git_blame = require('gitblame')
+local git_blame = require("gitblame")
 
 -- Setup.
 require("lualine").setup({
-    options = {
-        theme = "auto",
-        -- component_separators = { left = '', right = ''},
-        -- section_separators = { left = "", right = "" },
-        component_separators = { left = '', right = '' },
-        section_separators = { left = "", right = "" },
-        globalstatus = true,
-        disabled_filetypes = { "fzf" },
-    },
-    sections = {
-        lualine_a = {
-            {
-                "filename",
-                newfile_status = true,
-                path = 1,
-                symbols = {
-                    modified = "[+]",
-                    readonly = "[-]",
-                    unnamed = "[No Name]",
-                    newfile = "[New]",
-                },
-            },
+  options = {
+    theme = "spaceduck",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    globalstatus = true,
+    disabled_filetypes = { "fzf" },
+  },
+  sections = {
+    lualine_a = {
+      {
+        "filename",
+        newfile_status = true,
+        path = 1,
+        symbols = {
+          modified = "[+]",
+          readonly = "[-]",
+          unnamed = "[No Name]",
+          newfile = "[New]",
         },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {
-            { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
-            "branch",
-            "diff",
-            "diagnostics",
-        },
-        lualine_y = { "filetype", "filesize" },
-        lualine_z = { "progress" },
+      },
     },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {
+      { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+      "branch",
+      "diff",
+      "diagnostics",
+    },
+    lualine_y = { "filetype", "filesize" },
+    lualine_z = { "progress" },
+  },
 })
