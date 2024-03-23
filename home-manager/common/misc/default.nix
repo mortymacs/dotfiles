d4f70862839bb8b3fa1d339d9cfe85cfa5116159
Morteza NourelahiAlamdari <m@0t1.me>
2024-03-23 20:59:18 +0100

    Remove udiskie package

--- home-manager/common/misc/default.nix
+++ home-manager/common/misc/default.nix
@@ -5,18 +5,6 @@
       enable = true;
       systemdTarget = "sway-session.target";
     };
-
-    udiskie = {
-      enable = true;
-      tray = "auto";
-      settings = {
-        program_options = {
-          udisks_version = 2;
-          tray = true;
-        };
-        icon_names.media = [ "media-optical" ];
-      };
-    };
   };
 
   # Custom config.
