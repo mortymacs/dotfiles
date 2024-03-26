99bb0c4d868b5a469638f86ec42bc7e77fecb696
Morteza NourelahiAlamdari <m@0t1.me>
2024-03-26 08:31:19 +0100

    Upgrade jnv package

--- home-manager/common/custom/default.nix
+++ home-manager/common/custom/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
     pname = "jnv";
-    version = "v0.1.3";
+    version = "v0.2.0";
 
     src = fetchurl {
-        sha256 = "1b4h2nmqaa8i64h15in074kivp2bvyrlz03zw1dsl7lbc649f87a";
+        sha256 = "1z9i9hq297w20a7985q6jy6k9rkaj3imy19n0c1hykiia3asfk4z";
         url = "https://github.com/ynqa/jnv/releases/download/${version}/jnv-x86_64-unknown-linux-gnu.tar.xz";
     };
 
