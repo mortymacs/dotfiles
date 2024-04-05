3249628df4ba86c3a83c9d1f49d4f49020df788e
Morteza NourelahiAlamdari <m@0t1.me>
2024-04-05 11:32:17 +0200

    Enable wayland on flameshot package

--- home-manager/common/flameshot/default.nix
+++ home-manager/common/flameshot/default.nix
@@ -1,19 +1,31 @@
-{
+{ pkgs, ... }:
+let
+  # https://github.com/NixOS/nixpkgs/issues/292700#issuecomment-1974953531
+  flameshotWayland = pkgs.flameshot.overrideAttrs (oldAttrs: {
+    cmakeFlags = [ "-DUSE_WAYLAND_CLIPBOARD=1" ];
+    buildInputs = oldAttrs.buildInputs ++ [ pkgs.libsForQt5.kguiaddons ];
+  });
+in {
   services.flameshot = {
     enable = true;
+    package = flameshotWayland;
     settings = {
       General = {
         # General.
         disabledTrayIcon = true;
         showStartupLaunchMessage = false;
         checkForUpdates = false;
-
-        # UI.
+        historyConfirmationToDelete = false;
+        showHelp = false;
+        showMagnifier = true;
+        showSidePanelButton = false;
         uiColor = "#0f111b";
         drawColor = "#D81E5B";
+        buttons = "";
 
         # Save/Export.
         copyPathAfterSave = true;
+        copyAndCloseAfterUpload = false;
       };
     };
   };
