3e4c80633af8ede06cb043c8b899233c9e4924a8
Morteza NourelahiAlamdari <m@0t1.me>
2024-04-21 16:23:49 +0200

    Upgrade jnv package

--- home-manager/common/custom/default.nix
+++ home-manager/common/custom/default.nix
@@ -1,16 +1,17 @@
 { fetchurl, stdenv, lib }:
 
 stdenv.mkDerivation rec {
-    pname = "jnv";
-    version = "v0.2.0";
+  pname = "jnv";
+  version = "v0.2.2";
 
-    src = fetchurl {
-        sha256 = "1z9i9hq297w20a7985q6jy6k9rkaj3imy19n0c1hykiia3asfk4z";
-        url = "https://github.com/ynqa/jnv/releases/download/${version}/jnv-x86_64-unknown-linux-gnu.tar.xz";
-    };
+  src = fetchurl {
+    # nix-prefetch-url --type sha256 https://github.com/ynqa/jnv/releases/download/v0.2.2/jnv-x86_64-unknown-linux-gnu.tar.xz
+    sha256 = "0jnv9q1aacmgrx3i80d6g95kwflvz33ipfdpa87gm80mf7qnlzq8";
+    url = "https://github.com/ynqa/jnv/releases/download/${version}/jnv-x86_64-unknown-linux-gnu.tar.xz";
+  };
 
-    installPhase = ''
-        mkdir -p $out/bin
-        tar xf $src -C $out/bin --strip-components=1
-    '';
+  installPhase = ''
+    mkdir -p $out/bin
+    tar xf $src -C $out/bin --strip-components=1
+  '';
 }
