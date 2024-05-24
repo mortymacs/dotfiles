f0212c31c2b1b431b358435a55fae9f65724e87b
Morteza NourelahiAlamdari <m@0t1.me>
2024-05-24 17:30:00 +0200

    Add noto-fonts-emoji

--- nixos/common/font.nix
+++ nixos/common/font.nix
@@ -7,13 +7,14 @@ in {
       (unstable.nerdfonts.override { fonts = [ mono ]; })
       unstable.vazir-fonts
       unstable.lexend
+      unstable.noto-fonts-emoji
     ];
     fontconfig = {
       enable = true;
       defaultFonts = {
         serif = [ "Lexend" "Vazirmatn" ];
         sansSerif = [ "Lexend" "Vazirmatn" ];
-        monospace = [ "${mono} Nerd Font" ];
+        monospace = [ "${mono} Nerd Font" "Noto Color Emoji" ];
         emoji = [ "Noto Color Emoji" ];
       };
     };
