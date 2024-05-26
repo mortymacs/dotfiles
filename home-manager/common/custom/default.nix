8944f7b5182e54ed868d237dd86c98bb852c13c2
Morteza NourelahiAlamdari <m@0t1.me>
2024-05-26 16:33:38 +0200

    Upgrade packages

--- home-manager/common/custom/default.nix
+++ home-manager/common/custom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jnv";
-  version = "v0.2.2";
+  version = "v0.2.3";
 
   src = fetchurl {
     # nix-prefetch-url --type sha256 https://github.com/ynqa/jnv/releases/download/v0.2.2/jnv-x86_64-unknown-linux-gnu.tar.xz
-    sha256 = "0jnv9q1aacmgrx3i80d6g95kwflvz33ipfdpa87gm80mf7qnlzq8";
+    sha256 = "01dvz3yk6byln5yhl6h8dhhxclkf36wxx5n6zfwmxsmpydd0asih";
     url = "https://github.com/ynqa/jnv/releases/download/${version}/jnv-x86_64-unknown-linux-gnu.tar.xz";
   };
 
