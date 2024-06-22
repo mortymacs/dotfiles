e9ab3134177704a47724fe296ade0508ae2c1253
Morteza NourelahiAlamdari <m@0t1.me>
2024-06-22 19:46:09 +0200

    Update nvim diagnostic config

--- home-manager/common/nvim/lua/lua/diagnostic.lua
+++ home-manager/common/nvim/lua/lua/diagnostic.lua
@@ -1,7 +1,7 @@
 -- Config.
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
+  virtual_text = true,
+  signs = false,
   underline = false,
   update_in_insert = true,
   severity_sort = true,
@@ -10,11 +10,6 @@ vim.diagnostic.config({
     highighlight_whole_line = false,
   },
 })
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
 
 -- UI.
 local border = {
