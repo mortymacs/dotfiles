bff85277d335acda41f448ac90faf55a91245697
Morteza NourelahiAlamdari <m@0t1.me>
2024-08-24 15:54:42 +0200

    Add git-user-stats command

--- home-manager/common/git/default.nix
+++ home-manager/common/git/default.nix
@@ -1,18 +1,17 @@
+{ pkgs, ... }:
 {
   programs.git = {
     enable = true;
     userName = "Morteza NourelahiAlamdari";
     userEmail = "m@0t1.me";
     aliases = {
-      tree =
-        "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+      tree = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
       del-local = "branch -d";
       del-all = "branch -D";
       show-files = "diff-tree --no-commit-id --name-only -r";
       status-lines = "diff --stat";
       conflicts = "diff --name-only --diff-filter=U --relative";
-      restore-from-history =
-        ''!f(){ git checkout "$(git rev-list -n 1 HEAD -- $1)"^ -- "$1"; }; f'';
+      restore-from-history = ''!f(){ git checkout "$(git rev-list -n 1 HEAD -- $1)"^ -- "$1"; }; f'';
     };
 
     delta = {
@@ -26,7 +25,9 @@
     };
 
     extraConfig = {
-      push = { default = "current"; };
+      push = {
+        default = "current";
+      };
       pager = {
         diff = "delta";
         log = "delta";
@@ -34,12 +35,13 @@
         show = "delta";
         blame = "delta";
       };
-      help = { autocorrect = 10; };
+      help = {
+        autocorrect = 10;
+      };
       diff.algorithm = "histogram";
     };
 
-    ignores = map (v: "${toString v}")
-      (builtins.split "\n" (builtins.readFile ./ignore));
+    ignores = map (v: "${toString v}") (builtins.split "\n" (builtins.readFile ./ignore));
     includes = [
       { path = "./themes.gitconfig"; }
       {
@@ -57,4 +59,9 @@
     # Wallpaper.
     "git/themes.gitconfig".source = ./themes.gitconfig;
   };
+
+  home.packages = with pkgs; [
+    (writeShellScriptBin "git-user-stats" (builtins.readFile ./git-user-stats.sh))
+  ];
+
 }
