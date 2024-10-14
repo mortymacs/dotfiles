7efb1ccb8e42461e310f68c65eaea230b8a25666
Morteza NourelahiAlamdari <m@0t1.me>
2024-10-14 22:32:52 +0200

    Fix aws cli config initializer

--- home-manager/common/misc/default.nix
+++ home-manager/common/misc/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   # Services.
   services = {
@@ -20,18 +25,24 @@
     "clangd/config.yaml".source = ./clangd.yaml;
   };
 
-  # Check if AWS config file exists and conditionally add it.
-  home.file =
-    lib.optionalAttrs (!builtins.pathExists "${toString (config.home.homeDirectory)}/.aws/config")
-      {
-        ".aws/config".text = ''
-          [profile localhost]
-          region = us-east-1
-        '';
-        ".aws/credentials".text = ''
-          [localstack]
-          aws_access_key_id = foo
-          aws_secret_access_key = bar
-        '';
-      };
+  # AWS CLI config.
+  home.activation = {
+    createAwsConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [ ! -f "${config.home.homeDirectory}/.aws/config" ]; then
+        $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.aws"
+        $DRY_RUN_CMD cat > "${config.home.homeDirectory}/.aws/config" << EOF
+[profile localhost]
+region = us-east-1
+EOF
+      fi
+      if [ ! -f "${config.home.homeDirectory}/.aws/credentials" ]; then
+        $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.aws"
+        $DRY_RUN_CMD cat > "${config.home.homeDirectory}/.aws/credentials" << EOF
+[localstack]
+aws_access_key_id = foo
+aws_secret_access_key = bar
+EOF
+      fi
+    '';
+  };
 }
