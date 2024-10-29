ed2d76e5a6e2725d14bbccb8de4dc5da0998f39b
Morteza NourelahiAlamdari <m@0t1.me>
2024-10-29 12:52:59 +0100

    Add cg alias

--- home-manager/common/fish/aliases.nix
+++ home-manager/common/fish/aliases.nix
@@ -22,6 +22,7 @@
   # Search.
   f = "fzf --no-mouse --preview 'bat {} --style=numbers --color=always'";
   gg = "rg -i -n";
+  cg = "ast-grep run --pattern";
   gfi = "sk -i -c 'rg {}'";
 
   # Git.
