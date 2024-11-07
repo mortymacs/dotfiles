9de1b9d55c3837a1cc9d00992f1f93a90a3139b2
Morteza NourelahiAlamdari <m@0t1.me>
2024-11-07 09:50:48 +0100

    Update nvim gui config

--- home-manager/common/nvim/lua/lua/vim.lua
+++ home-manager/common/nvim/lua/lua/vim.lua
@@ -66,7 +66,7 @@ vim.opt.timeoutlen = 300
 if vim.g.neovide then
   vim.opt.winblend = 20
   vim.g.neovide_cursor_vfx_mode = "railgun"
-  vim.o.guifont = "monospace:h14"
+  vim.o.guifont = "monospace:h12"
   vim.g.neovide_transparency = 0.8
   vim.g.neovide_theme = "auto"
   vim.g.neovide_scroll_animation_length = 0.3
