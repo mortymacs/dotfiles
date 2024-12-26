3621fd53c0d35a7092f34361161fb630f55a7f52
Morteza NourelahiAlamdari <m@0t1.me>
2024-12-26 15:00:56 +0100

    Fix nvim terminal padding

--- home-manager/common/nvim/lua/lua/terminal.lua
+++ home-manager/common/nvim/lua/lua/terminal.lua
@@ -1,2 +1,16 @@
 -- https://stackoverflow.com/a/63908546/2338672
-vim.api.nvim_create_autocmd("TermOpen", { command = "setlocal nonu nornu signcolumn=no" })
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.diagnostic.disable(0)
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.foldcolumn = "0"
+    vim.opt_local.signcolumn = "no"
+    vim.opt_local.cursorcolumn = false
+    vim.opt_local.wrap = false
+    vim.opt_local.scrolloff = 0
+    vim.opt_local.sidescrolloff = 0
+    vim.opt_local.buflisted = false
+    vim.opt_local.cursorline = false
+  end,
+})
