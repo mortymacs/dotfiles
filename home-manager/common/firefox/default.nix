67ee7df1891e9f382a5725128403a09d60c25858
Morteza NourelahiAlamdari <m@0t1.me>
2025-06-06 08:57:38 +0200

    Update firefox config

--- home-manager/common/firefox/default.nix
+++ home-manager/common/firefox/default.nix
@@ -37,7 +37,7 @@
                   template = "https://nixos.wiki/index.php?search={searchTerms}";
                 }
               ];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              icon = "https://nixos.wiki/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = [ "@nw" ];
             };
