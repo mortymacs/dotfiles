b564bbe57d9d2fc8ed27e68f6f23999c1a8f2e08
Morteza NourelahiAlamdari <m@0t1.me>
2025-08-17 20:49:02 +0200

    Update neotree config

--- home-manager/common/editor/lua/lua/package.lua
+++ home-manager/common/editor/lua/lua/package.lua
@@ -54,6 +54,7 @@ require("lazy").setup({
   -- Finder.
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
