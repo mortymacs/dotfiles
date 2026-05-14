c4ac836031808c0f69a7822243265aa0729c8d6d
Morteza NourelahiAlamdari <m@0g1.org>
2026-05-14 12:16:59 +0200

    Update ai related config

--- home-manager/common/ai/default.nix
+++ home-manager/common/ai/default.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, ... }:
 {
   services.ollama = {
-    enable = false;
+    enable = true;
     acceleration = "cuda";
     host = "127.0.0.1";
     package = pkgs.unstable.ollama;
@@ -9,7 +9,11 @@
 
   home.packages = with pkgs; [
     unstable.crush
-    unstable.claude-code
     inputs.llama-cpp.packages.${pkgs.stdenv.hostPlatform.system}.cuda
+    unstable.claude-code
   ];
+
+  xdg.configFile = {
+    "crush/crush.json".source = ./crush.json;
+  };
 }
