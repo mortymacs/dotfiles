6ac16f97e077d8839fef2c4db77d19719b9218cb
Morteza NourelahiAlamdari <m@0g1.org>
2026-05-26 20:43:56 +0200

    Update work profile fish aliases

--- home-manager/work/misc/extra.fish
+++ home-manager/work/misc/extra.fish
@@ -5,4 +5,4 @@ set -x GOPRIVATE "gitlab.ci.fdmg.org/*"
 set -x MYCLI_HISTFILE "$HOME/.config/mycli/history"
 
 # Aliases.
-alias vpn "sudo cato-sdp start --append=tail --route 10.0.0.0/8 --reconn 5 --headless"
+alias vpn "cato-sdp start --append=tail --route 10.0.0.0/8 --reconn 5 --headless"
